492f7c8333202ef1f3a5d3ae5302b07a6775fa77
SeantheShort <gapammons@gmail.com>
2024-10-08 13:35:51 -0400

    Improved Main Menu and Transition

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -2,10 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    // Variables
+    private float alphaGoal = 0;
+    private float currentAlpha = 0;
+    
+    // Game Object References
+    public Image transitionScreen;
+
+    void Update()
+    {
+        // Updating Transition Screen Alpha
+        if (alphaGoal != 0)
+        {
+            // Lerping
+            currentAlpha = Mathf.MoveTowards(currentAlpha, alphaGoal, Time.deltaTime * 5);
+            transitionScreen.color = new Color(0.6431373f, 0.7843138f, 1, currentAlpha);
+            
+            // Changing Scene
+            if (Mathf.Abs(alphaGoal-currentAlpha) < 0.01f)
+                SceneManager.LoadScene("Gameplay");
+        }
+        
+    }
+
     // Quit Button
     public void QuitGame()
     {
@@ -14,8 +38,9 @@ public class ButtonManager : MonoBehaviour
     }
     
     // Scene Switch Button
-    public void SwitchScene(string sceneName)
+    public void SwitchScene()
     {
-        SceneManager.LoadScene(sceneName);
+        // Activating Transition
+        alphaGoal = 1;
     }
 }
--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    // Game Object References
     public Transform player;
     private Vector3 targetPosition;
     public float lerpSpeed;
-    
+    public Camera cam;
+
+    void Start()
+    {
+        cam.orthographicSize = 400;
+    }
+
     void FixedUpdate()
     {
+        // Camera view Transition
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 7, 5 * Time.deltaTime);
+        
+        // Lerping Camera to Object
         targetPosition = new Vector3(player.position.x, Mathf.Max(player.position.y, 0), player.position.z - 5);
         this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, lerpSpeed * Time.deltaTime);
     }
